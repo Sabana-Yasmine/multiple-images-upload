da197f5a4aa35457b9062da7822058e968248db4
unknown <you@example.com>
2022-08-26 19:25:15 +0530

    Third-commit

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const cors = require("cors");
-app.use(cors());
 const ejs = require("ejs");
 
-//app.use(express.static('views/upload'))
+app.use(express.static('upload'))
+let data=[];
 
 app.use(express.static("upload"));
 app.set("view engine","ejs");
 
 const storage = multer.diskStorage({
     destination : (req,file,cb)=>{
-        cb(null,"views/upload")
+        cb(null,"./views/upload")
     },
     filename : (req,file,cb)=>{
         cb(null,Date.now()+"."+file.originalname)
     }
 })
 
-
 const upload = multer({storage:storage}).array('file',5);
 
-
 app.post("/uploading_images", async(req,res)=>{
-    console.log("posting data");
+    
     try{
+        
+    console.log("posting data");
         upload(req,res,(err)=>{
             if(!req.files)
             {
@@ -41,13 +40,13 @@ app.post("/uploading_images", async(req,res)=>{
             }
             else
             {
-                //res.json({status:"success",data : req.files})
-               const data = req.files
+                //res.send({status:"success",data : req.files})
+                //for(let i of req.files){
+                    //console.log("data", i)
+                    //res.render('index',{data:i})}
+            const data = req.files
                res.render("index",{data:data})
-                
             }
-                 
-            
         })
     }catch(err){
         res.json({"err": err.message})
@@ -55,10 +54,10 @@ app.post("/uploading_images", async(req,res)=>{
 })
 
 app.get("/",(req,res)=>{
-    res.render("index")
+   res.render('index',{data})
 })
 
-app.listen(578,()=>{
+app.listen(5584,()=>{
     console.log("server created")
 })
 
